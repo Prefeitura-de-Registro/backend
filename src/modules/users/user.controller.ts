70f7f532e1408c139032a9764b94f2c3d72bca83
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-09-14 21:00:43 -0300

    feat: implementa rota de login de usuários

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from "express";
 import { generateToken } from "../../shared/utils/jwt.js";
 import { userService } from "./user.service.js";
-import { CreateUserDTO } from "./dtos/user.dto.js";
+import { CreateUserDTO, LoginUserDTO } from "./dtos/user.dto.js";
 
 export class UserController {
   register = async (request: Request, response: Response) => {
@@ -23,5 +23,30 @@ export class UserController {
       },
       token,
     });
+  };
+
+  login = async (request: Request, response: Response) => {
+    const data: LoginUserDTO = request.body;
+
+    const usuario = await userService.login(data);
+
+    const { id, email, name, tipoUsuario } = usuario;
+
+    const token = generateToken({ id, email, name, tipoUsuario });
+
+    response.status(200).json({
+      usuario: {
+        id: usuario.id,
+        nome: usuario.name,
+        email: usuario.email,
+        tipo_usuario: usuario.tipoUsuario,
+        created_at: usuario.createdAt,
+      },
+      token,
+    });
+  };
+
+  me = async (request: Request, response: Response) => {
+    response.status(200).json(request.user);
   };
 }
